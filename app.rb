676bddd7fe1d39b476d338b56e3f7bc93d03629b
isabella3412 <izzybella17@icloud.com>
2018-06-26 19:57:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
 
     if @operation == "add"
-      @number1 = @number2
+      @number1 + @number2
     elsif @operation == "subtract"
       @number1 - @number2
     elsif @operation == "multiply"
