ba7d78fa630cc64d5234bb1750de419e4aa3d4b4
isabella3412 <izzybella17@icloud.com>
2018-06-26 19:43:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name=params[:name]
+    @name.reverse
+  end
 
+  get '/square/:number' do
+      num = params[:number]
+      (num.to_i ** 2).to_s
+  end
+
+  get '/say/:number/:phrase' do 
+      @number = params[:number].to_i
+      @phrase = params[:phrase]
+      @phrase * @number
+    
 end
