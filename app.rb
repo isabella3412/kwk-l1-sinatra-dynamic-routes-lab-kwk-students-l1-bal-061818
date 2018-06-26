17d2d99b69114c888cd8a9f39db8a22603f8f85d
isabella3412 <izzybella17@icloud.com>
2018-06-26 20:14:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
     end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-"#{params[:word1]} #{{params[:word2]} #{params}"
-{params[:word2]}}
+"#{params[:word1]} #{{params[:word2]} 
+{params[:word2]}}"
   end
 
   get '/:operation/:number1/:number2' do
