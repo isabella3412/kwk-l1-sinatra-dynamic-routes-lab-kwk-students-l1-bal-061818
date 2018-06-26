7985550469398c57c23ba33871b85db81259d205
isabella3412 <izzybella17@icloud.com>
2018-06-26 19:51:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name=params[:name]
+    @name = params[:name]
     @name.reverse
   end
 
@@ -19,6 +19,29 @@ class App < Sinatra::Base
     end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+    @words = params[:word]
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    if @operation == "add"
+      @number1 = @number2
+    elsif @operation == "subtract"
+      @number1 - @number2
+    elsif @operation == "multiply"
+      @number1 * @number2
+    else
+      @number1 / @number2
+    end
+  end
+
+
+
+
+
+
+
 end
